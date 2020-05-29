78b202bec52e91e1843c22d2c26f62b6a13c80b6
onedeadhero <onedeadhero@gmail.com>
2020-05-29 01:54:30 -0400

    Weakauras skin no longer handles fonts.

--- ShestakUI/Modules/Skins/WeakAuras.lua
+++ ShestakUI/Modules/Skins/WeakAuras.lua
@@ -34,26 +34,6 @@ frame:SetScript("OnEvent", function()
 			frame.bar.fg:SetTexture(C.media.texture)
 			frame.bar.bg:SetTexture(C.media.texture)
 		end
-
-		if frame.stacks then
-			frame.stacks:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.stacks:SetShadowOffset(C.media.cooldown_timers_font_shadow and 1 or 0, C.media.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.text then
-			frame.text:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.text:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.text2 then
-			frame.text2:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.text2:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-		end
-
-		if frame.timer then
-			frame.timer:SetFont(C.font.cooldown_timers_font, C.font.cooldown_timers_font_size, C.font.cooldown_timers_font_style)
-			frame.timer:SetShadowOffset(C.font.cooldown_timers_font_shadow and 1 or 0, C.font.cooldown_timers_font_shadow and -1 or 0)
-		end
 	end
 
 	for weakAura, _ in pairs(WeakAuras.regions) do
